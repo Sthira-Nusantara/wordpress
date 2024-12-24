d8a105e91043d7b9347d686decb45637676d9cf7
fauzanfebrian <fauzanfebriann@gmail.com>
2024-12-24 13:35:04 +0700

    renaming

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@
 
 script_dir=$(dirname "$(readlink -f "$0")")
 
-docker compose -f "$script_dir/docker-compose.yaml" -p serviceyuk pull
-docker compose -f "$script_dir/docker-compose.yaml" -p serviceyuk build
+docker compose -f "$script_dir/docker-compose.yaml" -p wp pull
+docker compose -f "$script_dir/docker-compose.yaml" -p wp build
 docker compose -f "$script_dir/docker-compose.yaml" -p wp up -d
